f1f48125bac56b459920bed763680ff3e790679c
PavelVoitov <voitov.pavel@yandex.ru>
2023-06-26 02:15:35 +0300

    feat: added link for logo

--- client/src/shared/ui/Header/Header.tsx
+++ client/src/shared/ui/Header/Header.tsx
@@ -1,6 +1,7 @@
 import {FC, ReactNode} from "react"
 import OutlineBell from '../../../../public/icons/bell-outline.svg'
 import style from './Header.module.scss'
+import Link from "next/link";
 
 type HeaderProps = {
 	logo: string
@@ -11,11 +12,11 @@ export const Header: FC<HeaderProps> = ({logo, children}) => {
 	return (
   <header className={style.header}>
 		<div className={style.headerContainer}>
-			<div className={style.logo}>{logo}</div>
-			<div>
+				<Link href={'/'} className={style.link}>
+					{logo}
+				</Link>
 				<OutlineBell stroke="white" />
 				{children}
-			</div>
 		</div>
   </header>
 	)
