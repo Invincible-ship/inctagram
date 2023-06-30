c01114ccd1d83843f694d91646fdac57bd413d18
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-30 21:25:48 +0300

    feat(SignUpModalWindow change language): 1. add data to JSON change language 2. use t from useClientTranslation to change language

--- client/src/features/auth/signupModalWindow/ui/SignUpModalWindow.tsx
+++ client/src/features/auth/signupModalWindow/ui/SignUpModalWindow.tsx
@@ -7,19 +7,23 @@ import { Button } from '@/shared/ui/Button/Button'
 import '@/shared/styles/reset.scss'
 import '@/shared/styles/variables/common/_buttons.scss'
 import Link from 'next/link'
+import { useClientTranslation } from '@/shared/config/i18n/client'
 
 export const SignUpModalWindow = () => {
+
+	const { t } = useClientTranslation()
+
 	return (
 		<div className={s.wrapper}>
 			<div className={s.body}>
 				<div className={s.title}>
-					<h3>Email sent</h3>
+					<h3>{t('SignUpModal.title')}</h3>
 					<Link href={'#'}>
 						<span><Close /></span>
 					</Link>
 				</div>
 				<div className={s.content}>
-					<p className={s.text}>We have sent a link to confirm your email to epam@epam.com</p>
+					<p className={s.text}>{t('SignUpModal.text')} epam@epam.com</p>
 					<Link href={'#'} className={s.link}>
 						<	Button type='button' className={s.button}>OK</Button>
 					</Link>
