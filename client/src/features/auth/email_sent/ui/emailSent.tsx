4134a4635776b3ead6b14528a2e6876196960cd3
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-30 18:29:22 +0300

    feat & style(EmailSent): 1. add Link and Button 2. style Button

--- client/src/features/auth/email_sent/ui/emailSent.tsx
+++ client/src/features/auth/email_sent/ui/emailSent.tsx
@@ -1,6 +1,9 @@
 import s from './emailSent.module.scss'
 import Close from '@/shared/assets/icons/close.svg'
+import { Button } from '@/shared/ui/Button/Button'
 import '@/shared/styles/reset.scss'
+import '@/shared/styles/variables/common/_buttons.scss'
+import Link from 'next/link'
 
 export const EmailSent = () => {
 	return (
@@ -8,11 +11,15 @@ export const EmailSent = () => {
 			<div className={s.body}>
 				<div className={s.title}>
 					<h3>Email sent</h3>
-					<span><Close /></span>
+					<Link href={'#'}>
+						<span><Close /></span>
+					</Link>
 				</div>
 				<div className={s.content}>
 					<p className={s.text}>We have sent a link to confirm your email to epam@epam.com</p>
-					<button className={s.button}>OK</button>
+					<Link href={'#'} className={s.link}>
+						<	Button type='button' className={s.button}>OK</Button>
+					</Link>
 				</div>
 			</div>
 		</div>
