7b977d9f44154abb3af24b4530ab43b2abde9586
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-07-03 11:45:33 +0300

    feat(AccountMerging): 1. Отрисовываю разные рисунки в зависимости от сценария 2. Вынес Title в отдельную компоненту

--- client/src/features/auth/signup/ui/accountMerging/ui/accountMerging.tsx
+++ client/src/features/auth/signup/ui/accountMerging/ui/accountMerging.tsx
@@ -2,32 +2,71 @@
 
 import { Button } from '@/shared/ui/Button/Button'
 import s from './accountMerging.module.scss'
-import Pic from '@/shared/assets/icons/merge-image.svg'
+import PictureQuestion from '@/shared/assets/icons/merge-image.svg'
+import PictureDone from '@/shared/assets/icons/mergeDone-image.svg'
+import { ButtonTheme } from '@/shared/ui/Button/Button'
 
 export const AccountMerging = () => {
+	const changePageContent = {
+		merge: false,
+		//merge: true,
+		done: true,
+		//done: false
+	}
 	return (
 		<div className={s.accountMerging}>
 			<div className={s.wrapper}>
 				<div className={s.body}>
-					<div className={s.title}>
-						<h3>Merger of Accounts</h3>
-					</div>
+					<Title titleText='Merger of Accounts lorem' />
 					<div className={s.content}>
 						<p>The user with email Epam@epam.com is already in the system. Could we merge this accounts?</p>
 					</div>
-					<div className={s.buttons}>
-						<Button>Yes, merge</Button>
-						<Button>No</Button>
-					</div>
-					<div className={s.image}>
-						<div className={s.imageBox}>
-							<Pic
-							//viewBox="0 0 432 300" width="100%" className={s.picture}
-							/>
-						</div>
-					</div>
+					{changePageContent.merge &&
+						<div className={s.changinBox}>
+							<div className={s.buttons}>
+								<Button className={s.btn} theme={ButtonTheme.OUTLINED}>Yes, merge</Button>
+								<Button className={s.btn} theme={ButtonTheme.OUTLINED}>No</Button>
+							</div>
+							<div className={s.image}>
+								<PictureQuestion
+									viewBox="0 0 432 300" width="100%" //*! из-за этого я потратил целый день. так SVG адаптируется
+									className={s.picture}
+								/>
+								{/*<PictureDone
+									viewBox="0 0 432 300" width="100%"
+									className={s.picture}
+								/>*/}
+							</div>
+						</div>}
+					{changePageContent.done &&
+						<div className={s.changinBox}>
+							<div className={s.buttons}>
+								<Button className={s.btn} theme={ButtonTheme.DEFAULT}>Sing In</Button>
+							</div>
+							<div className={s.image}>
+								{/*<PictureQuestion
+									viewBox="0 0 432 300" width="100%" //*! из-за этого я потратил целый день. так SVG адаптируется
+									className={s.picture}
+								/>*/}
+								<PictureDone
+									viewBox="0 0 432 300" width="100%"
+									className={s.picture}
+								/>
+							</div>
+						</div>}
 				</div>
 			</div>
 		</div>
 	)
+}
+
+type TitleProps = {
+	titleText: string;
+}
+
+const Title = ({ titleText }: TitleProps) => {
+	return <div className={s.title}>
+		<h3>{titleText}</h3>
+		{/*<h3>Merger of Accounts lorem</h3>*/}
+	</div>
 }
