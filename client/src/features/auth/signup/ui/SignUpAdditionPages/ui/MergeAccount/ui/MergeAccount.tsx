e25b05692e4b9e8fd1e74bb6aa32b5a3eb34895c
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-07-03 21:19:37 +0300

    feat(SignUpAdditionPages): вынес CommonBlock, MergeAccount, Congratulation, Verification и styles в отдельные файлы. Пока не отрисовывал

---
+++ client/src/features/auth/signup/ui/SignUpAdditionPages/ui/MergeAccount/ui/MergeAccount.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react"
+import { CommonBlock, SignUpAdditionPagespProps } from "../../CommonBlock/ui/CommonBlock"
+import { useClientTranslation } from "@/shared/config/i18n/client"
+import { Button, ButtonTheme } from "@/shared/ui/Button/Button"
+import PictureQuestion from '@/shared/assets/icons/merge-image.svg'
+import s from './../../SignUpAdditionPagesStyles/SignUpAdditionPages.module.scss'
+
+
+export const MergeAccount: FC<SignUpAdditionPagespProps> = ({ lng }) => {
+	const { t } = useClientTranslation(lng, 'SignUpAdditionPages')
+	const emailExample = ' Email for example: Epam@epam.com '
+	return <>
+		<CommonBlock
+			email={emailExample}
+			textSecondPart={t('merge.text.textSecondPart')}
+			text={t('merge.text.textFirstPart')}
+			title={t('merge.title')} >
+			<div className={s.changinBox}>
+				<div className={s.buttons}>
+					<Button className={s.btn} theme={ButtonTheme.OUTLINED}>{t('merge.buttonText.yesMerge')}</Button>
+					<Button className={s.btn} theme={ButtonTheme.OUTLINED}>{t('merge.buttonText.no')}</Button>
+				</div>
+				<div className={s.image}>
+					<PictureQuestion
+						viewBox="0 0 432 300" width="100%" //*! из-за этого я потратил целый день. так SVG адаптируется
+						className={s.picture}
+					/>
+				</div>
+			</div>
+		</CommonBlock>
+	</>
+}
--- client/src/features/auth/signup/ui/SignUpAdditionPages/ui/SignUpAdditionPages.tsx
+++ client/src/features/auth/signup/ui/SignUpAdditionPages/ui/SignUpAdditionPages.tsx
@@ -9,7 +9,7 @@ import { ButtonTheme } from '@/shared/ui/Button/Button'
 import { useClientTranslation } from '@/shared/config/i18n/client'
 import { FC, ReactNode } from 'react'
 
-type SignUpAdditionPagespProps = {
+export type SignUpAdditionPagespProps = {
 	lng: string
 }
 
@@ -70,102 +70,12 @@ type TitleProps = {
 	titleText: string;
 }
 
-const Title = ({ titleText }: TitleProps) => {
+export const Title = ({ titleText }: TitleProps) => {
 	return <div className={s.title}>
 		<h3>{titleText}</h3>
 	</div>
 }
 
-type CommonBlockProps = {
-	title: string
-	text: string | any
-	textSecondPart?: string | any
-	children?: ReactNode
-	email?: string
-}
-
-const CommonBlock: FC<CommonBlockProps> = ({ email, title, text, textSecondPart, children }) => {
 
 
-	return <>
-		<div className={s.accountMerging}>
-			<div className={s.wrapper}>
-				<div className={s.body}>
-					<Title titleText={title} />
-					<div className={s.content}>
-						<p>{text} {email} {textSecondPart}</p>
-					</div>
-					{children}
-				</div>
-			</div>
-		</div>
-	</>
-}
 
-export const MergeAccount: FC<SignUpAdditionPagespProps> = ({ lng }) => {
-	const { t } = useClientTranslation(lng, 'SignUpAdditionPages')
-	const emailExample = ' Email for example: Epam@epam.com '
-	return <>
-		<CommonBlock
-			email={emailExample}
-			textSecondPart={t('merge.text.textSecondPart')}
-			text={t('merge.text.textFirstPart')}
-			title={t('merge.title')} >
-			<div className={s.changinBox}>
-				<div className={s.buttons}>
-					<Button className={s.btn} theme={ButtonTheme.OUTLINED}>{t('merge.buttonText.yesMerge')}</Button>
-					<Button className={s.btn} theme={ButtonTheme.OUTLINED}>{t('merge.buttonText.no')}</Button>
-				</div>
-				<div className={s.image}>
-					<PictureQuestion
-						viewBox="0 0 432 300" width="100%" //*! из-за этого я потратил целый день. так SVG адаптируется
-						className={s.picture}
-					/>
-				</div>
-			</div>
-		</CommonBlock>
-	</>
-}
-
-//export const MergeAccount: FC<SignUpAdditionPagespProps> = ({ lng }) => {
-export const Congratulation: FC<SignUpAdditionPagespProps> = ({ lng }) => {
-	const { t } = useClientTranslation(lng, 'SignUpAdditionPages')
-	return <>
-		<CommonBlock
-			text={t('congratulation.text')}
-			title={t('congratulation.title')} >
-			<div className={s.changinBox}>
-				<div className={s.buttons}>
-					<Button className={s.btn} theme={ButtonTheme.DEFAULT}>{t('congratulation.buttonText')}</Button>
-				</div>
-				<div className={s.image}>
-					<PictureDone
-						viewBox="0 0 432 300" width="100%"
-						className={s.picture}
-					/>
-				</div>
-			</div>
-		</CommonBlock>
-	</>
-}
-export const Verification: FC<SignUpAdditionPagespProps> = ({ lng }) => {
-	const { t } = useClientTranslation(lng, 'SignUpAdditionPages')
-	return <>
-		<CommonBlock
-			title={t('verification.title')}
-			text={t('verification.text')}
-		>
-			<div className={s.changinBox}>
-				<div className={s.buttons}>
-					<Button className={s.btn} theme={ButtonTheme.DEFAULT}>{t('verification.buttonText')}</Button>
-				</div>
-				<div className={s.image}>
-					<PictureVerification
-						viewBox="0 0 330 246" width="100%"
-						className={s.picture}
-					/>
-				</div>
-			</div>
-		</CommonBlock>
-	</>
-}
