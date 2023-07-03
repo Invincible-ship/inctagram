395e65150af2469bcd95c1081a70f21ef28b96c0
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-07-03 13:39:54 +0300

    порсто тест

--- client/src/features/auth/signup/ui/SignUpAdditionPages/ui/SignUpAdditionPages.tsx
+++ client/src/features/auth/signup/ui/SignUpAdditionPages/ui/SignUpAdditionPages.tsx
@@ -5,19 +5,32 @@ import s from './SignUpAdditionPages.module.scss'
 import PictureQuestion from '@/shared/assets/icons/merge-image.svg'
 import PictureDone from '@/shared/assets/icons/mergeDone-image.svg'
 import { ButtonTheme } from '@/shared/ui/Button/Button'
+import { useClientTranslation } from '@/shared/config/i18n/client'
+import { FC, ReactNode } from 'react'
 
-export const SignUpAdditionPages = () => {
+type SignUpAdditionPagespProps = {
+	lng: string
+}
+
+
+export const SignUpAdditionPages: FC<SignUpAdditionPagespProps> = ({ lng }) => {
+
 	const changePageContent = {
-		merge: false,
-		//merge: true,
-		done: true,
-		//done: false
+		//merge: false,
+		merge: true,
+		//done: true,
+		done: false
 	}
+
+	const { t } = useClientTranslation(lng, 'SignUpAdditionPages')
+
+
 	return (
 		<div className={s.accountMerging}>
 			<div className={s.wrapper}>
 				<div className={s.body}>
-					<Title titleText='Merger of Accounts' />
+					{changePageContent.merge && <Title titleText={t('title.merge')} />}
+					{changePageContent.done && <Title titleText={t('title.done')} />}
 					<div className={s.content}>
 						<p>The user with email Epam@epam.com is already in the system. Could we merge this accounts?</p>
 					</div>
@@ -44,10 +57,6 @@ export const SignUpAdditionPages = () => {
 								<Button className={s.btn} theme={ButtonTheme.DEFAULT}>Sing In</Button>
 							</div>
 							<div className={s.image}>
-								{/*<PictureQuestion
-									viewBox="0 0 432 300" width="100%" //*! из-за этого я потратил целый день. так SVG адаптируется
-									className={s.picture}
-								/>*/}
 								<PictureDone
 									viewBox="0 0 432 300" width="100%"
 									className={s.picture}
@@ -67,6 +76,36 @@ type TitleProps = {
 const Title = ({ titleText }: TitleProps) => {
 	return <div className={s.title}>
 		<h3>{titleText}</h3>
-		{/*<h3>Merger of Accounts lorem</h3>*/}
 	</div>
+}
+
+type Props = {
+	title: string
+	text: string | any
+	children?: ReactNode
+}
+
+const InfiBlock: FC<Props> = ({ title, text, children }) => {
+
+	return <>
+		<div className='title'>
+			{title}
+		</div>
+		<div className='text'>{text}</div>
+		<img></img>
+		{children}
+	</>
+}
+
+//button block
+
+const MergAccount: FC<{}> = () => {
+
+	return <>
+		<InfiBlock text="The user.." title='merg accounts' >
+			<button>X</button>
+			<button>Y</button>
+		</InfiBlock>
+	</>
+
 }
