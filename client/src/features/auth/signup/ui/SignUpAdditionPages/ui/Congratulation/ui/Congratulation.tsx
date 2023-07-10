47cfcda54ef5329f3edffc0321ccdfd5f6dea0ec
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-07-10 14:25:20 +0300

    feat (Congratulation) redirect to login

--- client/src/features/auth/signup/ui/SignUpAdditionPages/ui/Congratulation/ui/Congratulation.tsx
+++ client/src/features/auth/signup/ui/SignUpAdditionPages/ui/Congratulation/ui/Congratulation.tsx
@@ -1,30 +1,20 @@
 'use client'
 
-import { FC, useEffect, useState } from "react"
+import { FC } from "react"
 import { CommonBlock, SignUpAdditionPagespProps } from "../../CommonBlock/ui/CommonBlock"
 import { useClientTranslation } from "@/shared/config/i18n/client"
 import { Button, ButtonTheme } from "@/shared/ui/Button/Button"
 import PictureCongratulation from '@/shared/assets/icons/mergeDone-image.svg'
 import s from './../../SignUpAdditionPagesStyles/SignUpAdditionPages.module.scss'
-import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
+
 
 export const Congratulation: FC<SignUpAdditionPagespProps> = ({ lng }) => {
+    const router = useRouter();
 
-    const [confirmationCode, setConfirmationCode] = useState<string | null>(null)
-    const query = useSearchParams()
-
-    useEffect(() => {
-        if (query) {
-            const code = query.get('confirmationCode');
-            setConfirmationCode(code)
-        }
-    }, [query])
-
-    const registrationConfirmation = () => {
-        alert(confirmationCode)
+    const goToLogin = () => {
+        router.push('/login')
     }
-
-
 
     const { t } = useClientTranslation(lng, 'SignUpAdditionPages')
     return <>
@@ -33,10 +23,11 @@ export const Congratulation: FC<SignUpAdditionPagespProps> = ({ lng }) => {
             title={t('congratulation.title')} >
             <div className={s.changinBox}>
                 <div className={s.buttons}>
-                    <Button onClick={registrationConfirmation} className={s.btn} theme={ButtonTheme.DEFAULT}>{t('congratulation.buttonText')}</Button>
+                    <Button onClick={goToLogin} className={s.btn} theme={ButtonTheme.DEFAULT}>{t('congratulation.buttonText')}</Button>
                 </div>
                 <div className={s.image}>
                     <PictureCongratulation
+                        //! viewBox размеры по макету - hardcode
                         viewBox="0 0 432 300" width="100%"
                         className={s.picture}
                     />
