c202a9cfc0ee202dd5332f2937db3ff33887e87e
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-07-12 15:38:07 +0300

    refactor : routes congratulation & resendLink

---
+++ client/src/app/[lng]/resendLink/page.tsx
@@ -0,0 +1,5 @@
+export default function ResendLink() {
+    return <div>
+        <h1>resendLink</h1>
+    </div>
+} 
--- client/src/features/auth/signup/ui/SignUpAdditionPages/ui/EmailConfirmation/ui/EmailConfirmation.tsx
+++ client/src/features/auth/signup/ui/SignUpAdditionPages/ui/EmailConfirmation/ui/EmailConfirmation.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { FC, useEffect } from "react"
+import { FC, useEffect, useState } from "react"
 import { SignUpAdditionPagespProps } from "../../CommonBlock/ui/CommonBlock"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useEmailResendingMutation, useRegistrationConfirmationMutation } from "@/features/auth/signup/model/slice/rtkQslice"
 import { Congratulation } from "../../Congratulation/ui/Congratulation"
 import { Verification } from "../../Verification/ui/Verification"
+import { SignUpModal } from "../../../../modalWindow/ui/SignUpModal"
 
 
 export const EmailConfirmation: FC<SignUpAdditionPagespProps> = ({ lng }) => {
@@ -24,22 +25,54 @@ export const EmailConfirmation: FC<SignUpAdditionPagespProps> = ({ lng }) => {
 
     console.log(response);
 
+    const goToLogin = () => {
+        router.push('/login')
+    }
+    //========================================================================================================================================================
+    //for SignUpModal & Congratulations
+    const [isOpen, setIsOpen] = useState(false);
+    const onClose = () => {
+        setIsOpen(!isOpen)
+        goToLogin()
+    }
+
+    useEffect(() => {
+        if (response.isError && response.error.status === 400) {
+            setIsOpen(true);
+        }
+    }, [response.isError]);
 
     if (response.isLoading) {
-        return <div> <h2>Loading...</h2></div>
-    }
-    if (response.isSuccess) {
-        const goToLogin = () => {
-            router.push('/login')
-        }
-        return <Congratulation lng={lng} buttonAction={goToLogin} />
-    }
-    if (response.isError) {
-        const resendLink = () => {
-            resendLinkToEmail({ email: '84300krm@gmail.com' })
-            alert('resent link')
-        }
-        return <Verification lng={lng} buttonAction={resendLink} />
+        return <div><h2>Loading....</h2></div>
     }
 
+    //========================================================================================================================================================
+
+
+
+    //if (response.isLoading) {
+    //    return <div> <h2>Loading...</h2></div>
+    //}
+    //if (response.isSuccess) {
+    //    return <>
+    //        <Congratulation lng={lng} buttonAction={goToLogin} />
+    //    </>
+    //}
+    //if (response.isError && response.error.status === 400) {
+    //    console.log(response.error.status);
+    //    return <>
+    //        <h1>hello</h1>
+    //        <Congratulation lng={lng} buttonAction={goToLogin} />
+    //        <SignUpModal isOpen={isOpen} lng={lng} onClose={onClose} userEmail={'84300krm@gmail.com'} />
+    //    </>
+
+    //}
+    //if (response.isError) {
+    //    const resendLink = () => {
+    //        resendLinkToEmail({ email: '84300krm@gmail.com' }).unwrap()
+    //        alert('resent link')
+    //    }
+    //    return <Verification lng={lng} buttonAction={resendLink} />
+    //}
+    return <h1>hello</h1>
 }
