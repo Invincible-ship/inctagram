99d6657b87aa5e6263170a23bc9c882783eed18a
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-07-12 16:26:35 +0300

    refactor (EmailConfirmation): 1/ take status from url query 2/ if status === emailIsConfirmed render Congratulation 3/ if (status === emailWasUsed) render Congratulation & Modal

--- client/src/features/auth/signup/ui/SignUpAdditionPages/ui/EmailConfirmation/ui/EmailConfirmation.tsx
+++ client/src/features/auth/signup/ui/SignUpAdditionPages/ui/EmailConfirmation/ui/EmailConfirmation.tsx
@@ -3,76 +3,46 @@
 import { FC, useEffect, useState } from "react"
 import { SignUpAdditionPagespProps } from "../../CommonBlock/ui/CommonBlock"
 import { useRouter, useSearchParams } from "next/navigation"
-import { useEmailResendingMutation, useRegistrationConfirmationMutation } from "@/features/auth/signup/model/slice/rtkQslice"
 import { Congratulation } from "../../Congratulation/ui/Congratulation"
-import { Verification } from "../../Verification/ui/Verification"
 import { SignUpModal } from "../../../../modalWindow/ui/SignUpModal"
 
+const emailIsConfirmed = 'success'
+const emailWasUsed = 'confirm'
 
 export const EmailConfirmation: FC<SignUpAdditionPagespProps> = ({ lng }) => {
 
     const router = useRouter();
     const search = useSearchParams()
-    const [sendConfirmCode, response] = useRegistrationConfirmationMutation()
-    const [resendLinkToEmail] = useEmailResendingMutation()
+    const [status, setStatus] = useState<string | null>(null);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     useEffect(() => {
         if (search) {
-            const code = search.get('confirmationCode')
-            { code && sendConfirmCode(`confirmationCode=${code}`).unwrap() }
+            const queryStatus = search.get('status')
+            {
+                queryStatus === emailWasUsed
+                    ? setIsOpen(true)
+                    : null
+            }
+            { queryStatus && setStatus(queryStatus) }
         }
     }, [search])
-
-    console.log(response);
 
     const goToLogin = () => {
         router.push('/login')
     }
-    //========================================================================================================================================================
-    //for SignUpModal & Congratulations
-    const [isOpen, setIsOpen] = useState(false);
     const onClose = () => {
         setIsOpen(!isOpen)
         goToLogin()
     }
 
-    useEffect(() => {
-        if (response.isError && response.error.status === 400) {
-            setIsOpen(true);
-        }
-    }, [response.isError]);
-
-    if (response.isLoading) {
-        return <div><h2>Loading....</h2></div>
+    if (status === emailIsConfirmed) {
+        return <Congratulation buttonAction={goToLogin} lng={lng} />
     }
-
-    //========================================================================================================================================================
-
-
-
-    //if (response.isLoading) {
-    //    return <div> <h2>Loading...</h2></div>
-    //}
-    //if (response.isSuccess) {
-    //    return <>
-    //        <Congratulation lng={lng} buttonAction={goToLogin} />
-    //    </>
-    //}
-    //if (response.isError && response.error.status === 400) {
-    //    console.log(response.error.status);
-    //    return <>
-    //        <h1>hello</h1>
-    //        <Congratulation lng={lng} buttonAction={goToLogin} />
-    //        <SignUpModal isOpen={isOpen} lng={lng} onClose={onClose} userEmail={'84300krm@gmail.com'} />
-    //    </>
-
-    //}
-    //if (response.isError) {
-    //    const resendLink = () => {
-    //        resendLinkToEmail({ email: '84300krm@gmail.com' }).unwrap()
-    //        alert('resent link')
-    //    }
-    //    return <Verification lng={lng} buttonAction={resendLink} />
-    //}
-    return <h1>hello</h1>
+    else if (status === emailWasUsed) {
+        return <>
+            <Congratulation buttonAction={goToLogin} lng={lng} />
+            <SignUpModal lng={lng} onClose={onClose} isOpen={isOpen} userEmail={''} />
+        </>
+    }
 }
