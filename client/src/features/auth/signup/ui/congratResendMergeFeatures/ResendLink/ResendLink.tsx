bc83340dc357771d59e495a20b42c5f6335ccb20
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-08-01 11:37:01 +0300

    refactor by prettier

--- client/src/features/auth/signup/ui/congratResendMergeFeatures/ResendLink/ResendLink.tsx
+++ client/src/features/auth/signup/ui/congratResendMergeFeatures/ResendLink/ResendLink.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import { FC, useEffect, useState } from "react"
-import { congratResendMergePropsType } from "@/features/auth/signup/model/types/types"
-import { useClientTranslation } from "@/shared/config/i18n/client"
-import { ResendLinkModal } from "./ResendLinkModal/ResendLinkModal"
-import { useSearchParams } from "next/navigation"
-import { ErrorModal } from "./ResendLinkModal/ErrorModal"
-import { useEmailResendingMutation } from "@/features/auth/signup/model/api/signUpApi"
-import { Preloader } from "@/shared/ui/Preloader/Preloader"
-import { ResendLinkUI } from "./ui/ResendLinkUI"
+import { FC, useEffect, useState } from 'react'
+import { congratResendMergePropsType } from '@/features/auth/signup/model/types/types'
+import { useClientTranslation } from '@/shared/config/i18n/client'
+import { ResendLinkModal } from './ResendLinkModal/ResendLinkModal'
+import { useSearchParams } from 'next/navigation'
+import { ErrorModal } from './ResendLinkModal/ErrorModal'
+import { useEmailResendingMutation } from '@/features/auth/signup/model/api/signUpApi'
+import { Preloader } from '@/shared/ui/Preloader/Preloader'
+import { ResendLinkUI } from './ui/ResendLinkUI'
 
 const languageDatabase = 'signUpAdditionPages'
 const title = 'resendLink.title'
@@ -16,7 +16,6 @@ const text = 'resendLink.text'
 const buttonText = 'resendLink.buttonText'
 
 export const ResendLink: FC<congratResendMergePropsType> = ({ lng }) => {
-
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [email, setEmail] = useState<string>()
   const [noEmail, setNoEmail] = useState<boolean>(false)
@@ -27,12 +26,16 @@ export const ResendLink: FC<congratResendMergePropsType> = ({ lng }) => {
   useEffect(() => {
     if (search) {
       const queryEmail = search.get('email')
-      { queryEmail ? setEmail(queryEmail) : setNoEmail(true) }
+      {
+        queryEmail ? setEmail(queryEmail) : setNoEmail(true)
+      }
     }
   }, [search])
 
   useEffect(() => {
-    { (isSuccess || isError) && setIsOpen(true) }
+    {
+      ;(isSuccess || isError) && setIsOpen(true)
+    }
   }, [isSuccess, isError])
 
   const onClose = () => {
@@ -40,22 +43,28 @@ export const ResendLink: FC<congratResendMergePropsType> = ({ lng }) => {
   }
 
   const resendLink = () => {
-    { email ? sendLinkAgain({ email: email }) : setIsOpen(true) }
+    {
+      email ? sendLinkAgain({ email: email }) : setIsOpen(true)
+    }
   }
 
   if (isLoading) {
     return <Preloader />
   }
 
-  return <>
-    <ResendLinkUI
-      title={t(title)}
-      text={t(text)}
-      action={resendLink}
-      buttonText={t(buttonText)}
-    />
-    {noEmail && <ErrorModal lng={lng} isOpen={isOpen} onClose={onClose} />}
-    {isError && <ErrorModal lng={lng} isOpen={isOpen} onClose={onClose} userEmail={email} />}
-    {isSuccess && <ResendLinkModal lng={lng} isOpen={isOpen} onClose={onClose} userEmail={email} />}
-  </>
+  return (
+    <>
+      <ResendLinkUI
+        title={t(title)}
+        text={t(text)}
+        action={resendLink}
+        buttonText={t(buttonText)}
+      />
+      {noEmail && <ErrorModal lng={lng} isOpen={isOpen} onClose={onClose} />}
+      {isError && <ErrorModal lng={lng} isOpen={isOpen} onClose={onClose} userEmail={email} />}
+      {isSuccess && (
+        <ResendLinkModal lng={lng} isOpen={isOpen} onClose={onClose} userEmail={email} />
+      )}
+    </>
+  )
 }
