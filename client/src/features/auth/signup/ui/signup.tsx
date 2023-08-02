25d249e701fbf2d4e066e54dab943642da5bd9b6
arapbaeva <aiymvector@gmail.com>
2023-08-02 22:47:48 +0600

    fix: Changed lng to "" in signup

--- client/src/features/auth/signup/ui/signup.tsx
+++ client/src/features/auth/signup/ui/signup.tsx
@@ -1,67 +1,67 @@
 'use client'
-import { FC } from 'react'
-import { SignUpForm } from './SignUpForm'
-import { SocialButtons } from '@/features/auth/signup/ui/SocialButtons'
+import {FC} from 'react'
+import {SignUpForm} from './SignUpForm'
+import {SocialButtons} from '@/features/auth/signup/ui/SocialButtons'
 import {SubmitHandler, useForm} from 'react-hook-form'
-import { useClientTranslation } from '@/shared/config/i18n/client'
+import {useClientTranslation} from '@/shared/config/i18n/client'
 import '@/shared/styles/variables/common/_form.scss'
 import '@/shared/styles/variables/common/_b-titles.scss'
 import style from './signup.module.scss'
 import {
-  formSchema,
-  FormSchemaType,
+    formSchema,
+    FormSchemaType,
 } from '@/features/auth/signup/lib/validationConstants/validationConstants'
-import { zodResolver } from '@hookform/resolvers/zod'
+import {zodResolver} from '@hookform/resolvers/zod'
 import Link from 'next/link'
-import { Preloader } from '@/shared/ui/Preloader/Preloader'
-import { signupThunk } from '@/features/auth/signup/model/signup'
-import { useSelector } from 'react-redux'
-import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch'
+import {Preloader} from '@/shared/ui/Preloader/Preloader'
+import {signupThunk} from '@/features/auth/signup/model/signup'
+import {useSelector} from 'react-redux'
+import {useAppDispatch} from '@/shared/lib/hooks/useAppDispatch/useAppDispatch'
 
 export const SignUp = () => {
-  const isLoading = useSelector(state => state.signup.isLoading)
-  const dispatch = useAppDispatch()
-  const { t } = useClientTranslation("", 'signUp')
-  const schema = formSchema(t)
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<FormSchemaType>({
-    resolver: zodResolver(schema),
-  })
+    const isLoading = useSelector(state => state.signup.isLoading)
+    const dispatch = useAppDispatch()
+    const {t} = useClientTranslation("", 'signUp')
+    const schema = formSchema(t)
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+        setError,
+    } = useForm<FormSchemaType>({
+        resolver: zodResolver(schema),
+    })
 
-  const onSubmit:SubmitHandler<FormSchemaType> = data => {
-    dispatch(signupThunk({ body: data, setError }))
-  }
+    const onSubmit: SubmitHandler<FormSchemaType> = data => {
+        dispatch(signupThunk({body: data, setError}))
+    }
 
-  if (isLoading) {
-    return <Preloader />
-  }
+    if (isLoading) {
+        return <Preloader/>
+    }
 
-  return (
-    <div className={'form registration'}>
-      <div className="form-wrapper auth-form">
-        <div className={'title b-title bt26 semibold align-center'}>{t('signUp')}</div>
-        <SocialButtons />
-        <SignUpForm
-          onSubmit={handleSubmit(onSubmit)}
-          isLoading={isLoading}
-          t={t}
-          errors={errors}
-          register={register}
-        />
-        <span className={`info b-title bt14  align-center semibold`}>
+    return (
+        <div className={'form registration'}>
+            <div className="form-wrapper auth-form">
+                <div className={'title b-title bt26 semibold align-center'}>{t('signUp')}</div>
+                <SocialButtons/>
+                <SignUpForm
+                    onSubmit={handleSubmit(onSubmit)}
+                    isLoading={isLoading}
+                    t={t}
+                    errors={errors}
+                    register={register}
+                />
+                <span className={`info b-title bt14  align-center semibold`}>
           {t('doYouHaveAnAccount')}
         </span>
-        <Link
-          href={'/signIn'}
-          className={`b-title bt16 semibold ${style.linkRegistration} align-center`}
-        >
-          <span>{t('signIn')}</span>
-        </Link>
-      </div>
-    </div>
-  )
+                <Link
+                    href={'/signIn'}
+                    className={`b-title bt16 semibold ${style.linkRegistration} align-center`}
+                >
+                    <span>{t('signIn')}</span>
+                </Link>
+            </div>
+        </div>
+    )
 }
