09c2bbd6345854eda0d8a545cdfe903a64cbdafd
Andrey35211 <serand3521@yandex.ru>
2023-08-14 23:14:25 +0700

    update project

--- client/src/app/[lng]/page.tsx
+++ client/src/app/[lng]/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ResetPage } from "@/pages/resetPage"
 import { ForgotPage } from "@/pages/forgotPasswordPage"
+import {SignUpPage} from '@/pages/SignUpPage'
 
-// export default ResetPage
-export default ForgotPage
+export default ResetPage
+// export default ForgotPage
+// export default SignUpPage
 
 // const Page = () => {
 //   return (
