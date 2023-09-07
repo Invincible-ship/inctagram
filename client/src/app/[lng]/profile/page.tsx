8bfbf3cf17e8ea6fc52d1a5753db842d381fd3df
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2023-09-07 22:51:41 +0300

    add SignUpModal

--- client/src/app/[lng]/profile/page.tsx
+++ client/src/app/[lng]/profile/page.tsx
@@ -1,3 +1,9 @@
-export default function Profile() {
-  return <h1>Profile</h1>
+'use client'
+
+import { withAuth } from '@/shared/lib/HOC/withAuth/withAuth'
+
+const Profile = () => {
+  return <h1>Profile Page</h1>
 }
+
+export default withAuth(Profile, { routeRole: 'all' })
