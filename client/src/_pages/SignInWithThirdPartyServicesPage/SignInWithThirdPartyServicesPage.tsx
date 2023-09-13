0184e75dfe291ad5fd5e1b4a8e642b135196ff57
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2023-09-13 14:25:20 +0300

    fix isAuth in signInWithThirdPartyServicesPage

--- client/src/_pages/SignInWithThirdPartyServicesPage/SignInWithThirdPartyServicesPage.tsx
+++ client/src/_pages/SignInWithThirdPartyServicesPage/SignInWithThirdPartyServicesPage.tsx
@@ -29,7 +29,7 @@ export const SignInWithThirdPartyServicesPage: FC<SignInWithThirdPartyServicesPa
 
   const checkOAuth = useCallback(() => {
     if (isSuccess) {
-      const { isAuth, accessToken, user } = signInOAuthResponse
+      const { isAuth = false, accessToken, user } = signInOAuthResponse
 
       dispatch(setAuthData({ accessToken, user }))
 
