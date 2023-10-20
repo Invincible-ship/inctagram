7892d8381a95bb2deccf73592eb3e99165a9fb50
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2023-10-20 19:16:40 +0300

    fix SignIn, SignUp stories

--- client/src/features/auth/signIn/ui/SignIn.stories.tsx
+++ client/src/features/auth/signIn/ui/SignIn.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Meta, StoryObj } from '@storybook/react'
 import { SignIn } from './signIn'
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const meta: Meta<typeof SignIn> = {
   title: 'Components/SignIn',
@@ -13,5 +14,9 @@ export default meta
 type Story = StoryObj<typeof SignIn>
 
 export const SignInComponent: Story = {
-  render: () => <SignIn />,
+  render: () => (
+    <GoogleOAuthProvider clientId="">
+      <SignIn />
+    </GoogleOAuthProvider>
+  ),
 }
