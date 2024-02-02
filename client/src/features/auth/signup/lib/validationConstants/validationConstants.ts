7c6c18e22c21eb41641f946d41ffc713fc58163d
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2024-02-02 16:27:59 +0300

    fix whitespace in input; fix password validation

--- client/src/features/auth/signup/lib/validationConstants/validationConstants.ts
+++ client/src/features/auth/signup/lib/validationConstants/validationConstants.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 const userNameRequired = 'validate.userNameRequired'
 const userNameMinLength = 'validate.userNameMinLength'
 const userNameMaxLength = 'validate.userNameMaxLength'
+const userNameCanContain = 'validate.userNameCanContain'
 const emailInvalid = 'validate.emailInvalid'
 const emailRequired = 'validate.emailRequired'
 const passwordRequired = 'validate.passwordRequired'
@@ -22,7 +23,9 @@ export const formSchema = (t: TFunction<string, undefined>) =>
         .string()
         .min(1, { message: t(userNameRequired) })
         .min(6, { message: t(userNameMinLength) })
-        .max(30, { message: t(userNameMaxLength) }),
+        .max(30, { message: t(userNameMaxLength) })
+        .regex(/^[a-zA-Z0-9_-]*$/, t(userNameCanContain))
+        .trim(),
       email: z
         .string()
         .min(1, { message: t(emailRequired) })
@@ -32,9 +35,13 @@ export const formSchema = (t: TFunction<string, undefined>) =>
         .min(1, { message: t(passwordRequired) })
         .min(6, { message: t(passwordMinLength) })
         .max(20, { message: t(passwordMaxLength) })
-        .regex(/^(?=.*[A-Za-z])(?=.*[! "#$%&'()*+,-./:;<=>?@[\]^_`{|}~])/, {
-          message: t(passwordMustContain),
-        }),
+        .regex(
+          // eslint-disable-next-line max-len
+          /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}~])[A-Za-z0-9!"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}~]+$/,
+          {
+            message: t(passwordMustContain),
+          },
+        ),
       passwordConfirmation: z.string().min(1, { message: t(passwordConfirmationRequired) }),
     })
     .refine(data => data.password === data.passwordConfirmation, {
