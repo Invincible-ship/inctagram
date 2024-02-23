34b7421988ece561c7cbb342e54778a373e7510d
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2024-02-23 18:12:56 +0300

    fix: imports in TestAsyncThunk component

--- client/src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.tsx
+++ client/src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.tsx
@@ -1,6 +1,6 @@
 import { AsyncThunkAction } from '@reduxjs/toolkit'
 import axios, { AxiosStatic } from 'axios'
-import { StateSchema } from '@/providers/StoreProvider'
+import { StateSchema } from '@/app/providers/StoreProvider'
 
 type ActionCreatorType<Return, Arg, RejectedValue> = (
   arg: Arg,
